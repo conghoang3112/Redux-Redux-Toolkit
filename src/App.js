bb5a1e866fb3f317acca92335ceea4d5f62eb6bd
Cong Hoang <conghoang3112xxx@gmail.com>
2024-10-31 15:32:55 +0700

    Using useSelector, useDispatch - react-redux

--- src/App.js
+++ src/App.js
@@ -7,35 +7,50 @@ import {
   decreaseCounter,
 } from "./action/actions"
 
+import { useSelector, useDispatch } from "react-redux";
 
 function App(props) {
+
+  const dispatch = useDispatch();
+  const newCount = useSelector(
+    (state) => state.counter.count
+  );
+
+  //event handle
+  const handleIncrease = () => {
+    //dispatch action
+    // props.increaseCounter();
+    dispatch(increaseCounter());
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world with React!</h1>
-        <div>Count: {props.count}</div>
+        <div>Count: {newCount}</div>
 
-      <button onClick={() => props.increaseCounter()}>Increase Count</button>
+      <button onClick={() => handleIncrease()}>Increase Count</button>
 
-      <button onClick={() => props.decreaseCounter()}>Decrease Count</button>
+      <button onClick={() => dispatch(decreaseCounter())}>Decrease Count</button>
       </header>
     </div>
   );
 }
 
-const mapStateToProps = state => {
-  return {
-    count: state.counter.count,
-  }
-}
+// const mapStateToProps = state => {
+//   return {
+//     count: state.counter.count,
+//   }
+// }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    increaseCounter: () => dispatch(increaseCounter()),
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     increaseCounter: () => dispatch(increaseCounter()),
 
-    decreaseCounter: () => dispatch(decreaseCounter()),
-  }
-}
+//     decreaseCounter: () => dispatch(decreaseCounter()),
+//   }
+// }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+// export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
